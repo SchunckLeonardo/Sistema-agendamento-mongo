fc7afcb30a6f4d75afff5b496e8dd08e6e4b1c56
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-09-20 17:06:44 -0300

    creating Appointment model

---
+++ models/Appointment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+let appointment = new mongoose.Schema({
+    name: String,
+    email: String,
+    cpf: String,
+    description: String,
+    date: Date,
+    time: String
+})
+
+module.export = appointment
